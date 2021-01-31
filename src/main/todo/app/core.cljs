ae996345943ff4ede9a799fc61fa5e97ff199a76
dviramontes <dviramontes@gmail.com>
2021-01-31 13:34:08 -0700

    wip todo-input component

--- src/main/todo/app/core.cljs
+++ src/main/todo/app/core.cljs
@@ -1,18 +1,39 @@
 (ns todo.app.core
   (:require [reagent.dom :as rdom]
-            [todo.app.db :refer [db]]))
+            [reagent.core :as r]
+            [todo.app.db :as db]))
 
-(defn todo-item [title]
-  [:li [:span title]])
+(defn todo-item [todo]
+  [:li [:span (:title todo)]])
+
+(defn save [title]
+  (js/console.log "--save todo =>" title))
+
+(defn todo-input [on-save]
+  (let [value (r/atom "")]
+   (fn []
+     [:input
+      {:type "text"
+       :value @value
+       :on-key-down
+       #(case (.-which %)
+              13 (on-save @value) ;; enter-key
+              nil)
+       :on-change
+       #(reset! value (-> % .-target .-value))}])))
 
 (defn todo-wrapper []
-  [:div#todo-wrapper
-   [:header
-    [:h1 "To-Do Items:"]]
-   [:div#todos
-    (for [todo (:todos @db)]
-      ^{:key (gensym todo)}
-      [todo-item todo])]])
+  (let [todos (vals @db/todos)]
+   (fn []
+     [:div#todo-wrapper
+      [:header
+       [:h1 "To-Do Items:"]]
+      [:div#todos
+       [todo-input save]
+       (for [todo todos
+             :let [id (:id todo)]]
+          ^{:key (gensym id)}
+          [todo-item todo])]])))
 
 (defn chart-wrapper []
   [:div#chart-wrapper
@@ -33,4 +54,5 @@
    (js/document.getElementById "root")))
 
 (defn ^:export main []
+  (db/seed)
   (mount))
--- src/main/todo/app/db.cljs
+++ src/main/todo/app/db.cljs
@@ -1,6 +1,18 @@
 (ns ^:dev/once todo.app.db
   (:require [reagent.core :as r]))
 
-(def db 
-  (r/atom 
-   {:todos ["laundry" "dishes" "exercise"]}))
+(def init-todos
+  [{:id 0 :title "dishes" :done true}
+   {:id 1 :title "exercise" :done false}
+   {:id 2 :title "laundry" :done true}])
+
+(def todos
+  (r/atom (sorted-map)))
+
+(def counter
+  (r/atom 0))
+
+(defn seed []
+  (doseq [todo init-todos
+    :let [id (swap! counter inc)]]
+    (swap! todos assoc id todo)))
